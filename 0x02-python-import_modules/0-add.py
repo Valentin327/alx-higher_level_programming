b4cde2c22c6b32a48c294ad98e39ca922a61f08b
ValentinZONGO <valentizongo@gmail.com>
2022-06-30 13:41:52 -0700

    Import a simple function from a simple file, script update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print("<a {}> + <b {}> = ".format(a, b), end="")
-result = add_0.add(a, b)
-print("<add({}, {}) {}>".format(a, b, result))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("<a {}> + <b {}> = ".format(a, b), end="")
+    result = add(a, b)
+    print("<add({}, {}) {}>".format(a, b, result))
