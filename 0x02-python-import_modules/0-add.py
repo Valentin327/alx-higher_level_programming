a3eac271ef588e698227837edab2d26236aa1906
ValentinZONGO <valentizongo@gmail.com>
2022-06-30 13:45:25 -0700

    script update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("<a {}> + <b {}> = ".format(a, b), end="")
-    result = add(a, b)
-    print("<add({}, {}) {}>".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
