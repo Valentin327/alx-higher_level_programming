d06e0c1959cd493c39b90170390af50db64a797a
ValentinZONGO <valentizongo@gmail.com>
2022-07-01 06:25:38 -0700

    update script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,10 @@
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
-    if argc == 0:
+    if argc == 1:
         print("{} arguments.".format(argc - 1))
+    elif argc == 2:
+        print("{} arguments:".format(argc - 1))
     else:
         print("{} arguments:".format(argc - 1))
         for i in range(1, argc):
