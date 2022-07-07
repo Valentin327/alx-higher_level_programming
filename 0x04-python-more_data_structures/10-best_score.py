b6c4bca1a0ce356f087f14a0f3901f61e66a50b7
ValentinZONGO <valentizongo@gmail.com>
2022-07-07 06:30:46 -0700

    best score script

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    return (max(a_dictionary))
+    max_s = 0
+    for key, value in a_dictionary.items():
+        if value > max_s:
+            max_s = value
+    for key, value in a_dictionary.items():
+        if max_s == value:
+            return (key)
