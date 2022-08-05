7f954265ac709abb6f1990e5b2831e2c5da64b6b
ValentinZONGO <valentizongo@gmail.com>
2022-08-04 18:04:58 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Define a class Square with size"""
+class Square:
+    """class private instance attribute"""
+    def __init__(self, size):
+        self._Square__size = size
