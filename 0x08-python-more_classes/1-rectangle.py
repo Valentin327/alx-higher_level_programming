31c7878079468bc4d82f1191479a66d83209be56
ValentinZONGO <valentizongo@gmail.com>
2022-08-10 09:26:06 -0700

    sixth update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,13 +25,9 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """setter for
-        rectangle width
-        property
-        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if(value < 0):
+        if (value < 0):
             raise ValueError("width must be >= 0")
         self._Rectangle__height = value
 
@@ -49,10 +45,6 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """setter for
-        rectangle height
-        property
-        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if(value < 0):
