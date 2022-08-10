bcc15b3b7cbf1476278deb93fb3e9eff446716f7
ValentinZONGO <valentizongo@gmail.com>
2022-08-10 09:11:18 -0700

    fourth update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
         rectangle width
         property
         """
-        if (type(value) is not int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if(value < 0):
             raise ValueError("width must be >= 0")
@@ -52,7 +52,7 @@ class Rectangle:
         rectangle height
         property
         """
-        if (type(value) is not int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if(value < 0):
             raise ValueError("height must be >= 0")
