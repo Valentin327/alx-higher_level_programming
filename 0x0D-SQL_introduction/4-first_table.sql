03d018e191f8c80e0a0995a0901dec1ff3c5ed64
ValentinZONGO <valentizongo@gmail.com>
2022-09-06 05:12:35 -0700

    first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- create table in the current database in your mysql server
 -- create table in the current database
-IF NOT EXISTS (CREATE TABLE first_table(id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
