b2b88a897c2357a4ea121d44bb5ba3d26ba89b6e
ValentinZONGO <valentizongo@gmail.com>
2022-10-01 16:02:44 -0700

    Lookup function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module 0-lookup
+return the list of available attributes
+"""
+
+
+def lookup(obj):
+    """ function that return the list
+    of available attributes and methods
+    of an object
+    """
+
+return dir(obj)
